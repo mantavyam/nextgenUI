3e4883b1c1826ce78f31387796c9803a6dd010c5
Shivam <mantavyamindia@gmail.com>
2024-11-20 01:39:37 +0530

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
+import 'assets.dart';
+import 'title_screen/title_screen.dart';
+
+void main() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowMinSize(const Size(800, 500));
+  }
+  Animate.restartOnHotReload = true;
+  runApp(                                                // Edit from here...
+    FutureProvider<FragmentPrograms?>(
+      create: (context) => loadFragmentPrograms(),
+      initialData: null,
+      child: const NextGenApp(),
+    ),
+  );                                                     // to here.
+}
+
+class NextGenApp extends StatelessWidget {
+  const NextGenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      home: const TitleScreen(),
+    );
+  }
+}
